3714674c0b1ce41560b3867dfe8cb4660e12532b
Shloka Bhalgat <shlokabhalgat@192.168.1.4>
2022-01-29 14:52:28 +0530

    Initial commit

---
+++ poochshop_apps/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PoochshopAppsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'poochshop_apps'
---
+++ poochshop_apps/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index),
+    path('dashboard', views.dashboard),
+    path('logout', views.logout),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('social_django.urls')),
+]
